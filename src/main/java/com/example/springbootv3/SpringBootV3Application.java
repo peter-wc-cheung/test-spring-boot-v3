6a34aef30e0ffed48edb2413ee0c7ba8e57385dc
Peter Cheung <peter@6uohk.com>
2022-11-17 00:13:24 +0000

    init.

---
+++ src/main/java/com/example/springbootv3/SpringBootV3Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootV3Application.class, args);
+	}
+
+}
